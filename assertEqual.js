056544ea791ac35d80f24c5e967d13df66f7704a
twoto22 <goktug>
2021-10-27 11:28:45 +0000

    emoji

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const aAE = function(array1, array2){
+  if ((array1, array2) === true) {
+      console.log(`✅✅✅ Assertion Passed: ${array1} is equal to ${array2}`);
+ 
+   }else {
+      console.log(`❌❌❌ Assertion Failed: ${array1} is not equal to ${array2}`);
+
+  }
+};
+
+aAE("Lighthouse Labs", "Bootcamp");
+aAE(1, 1);
