dec51ea8857d18c694b45628d1b942bef0f72504
twoto22 <goktug>
2021-10-29 20:45:38 +0000

    position

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const New = sentence[i]
+    if(results[New]) {
+      results[New].push(i)
+    } else {
+      results[New] = [i]
+    }
+    
+  }
+  console.log(results);
+
+
+
+
+
+  return results;
+};
+
+
+const assertEqual = function(array1, array2){
+  if (array1 === array2) {
+      console.log(`✅✅✅ Assertion Passed: ${array1} is equal to ${array2}`);
+ 
+   } else {
+      console.log(`❌❌❌ Assertion Failed: ${array1} is not equal to ${array2}`);
+
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) return false;
+  for (let element = 0; element < array1.length; element += 1) {
+    if (Array.isArray(array1[element]) || Array.isArray(array2[element])) {
+      output = output && eqArrays(array1[element], array2[element]);
+    } else if (array1[element] !== array2[element]) {
+      output = output && false;
+    }
+  }
+  return output;
+};
+
+module.exports = eqArrays;
+
+
+letterPositions("check")
