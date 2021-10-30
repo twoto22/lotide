7903e7ae016de62c4b7dc32e22d2514650d726c3
twoto22 <goktug>
2021-10-30 23:21:51 +0000

    words, word =>world

---
+++ map.js
@@ -0,0 +1,38 @@
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g','c','t','m','t'])
+
+const results2 = map(words, word => word[3]);
+assertArraysEqual(results2, ['u','t', undefined, 'o', undefined]);
+
+const results3 = map(words, word => word.length);
+assertArraysEqual(results3, [6, 6, 2, 5, 3]);
